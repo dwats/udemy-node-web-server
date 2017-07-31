92f8b57ac6f4662f65570266dc5e36de6eed57e7
Eric Julius <ejulius86@gmail.com>
2017-07-30 22:30:58 -0500

    Setup npm start sscript and heroku port.

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const hbs = require('hbs');
 const path = require('path');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 const app = express();
 hbs.registerPartials(path.join(__dirname, '/views/partials'));
 app.set('view engine', 'hbs');
@@ -41,6 +42,6 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
